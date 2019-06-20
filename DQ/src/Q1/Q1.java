c27e75ec066a4b0e33742fb04901a6a189d552ad
Wolf <Wolf@DESKTOP-21SVAEG>
2019-06-20 11:44:55 +0900

    Q1

--- DQ/src/Q1/Q1.java
+++ DQ/src/Q1/Q1.java
@@ -1,5 +1,25 @@
 package Q1;
 
 public class Q1 {
+	public static void main(String arg[]) {
+		Q1 q =new Q1();
+		q. Multiple(10000);
+
+	}
+	public void  Multiple(int max) {
+		int i = 1;
+		int sum = 0;
+		while(i < max) {
+			if(i % 3 == 0 || i % 5 == 0) {
+				sum += i; 
+				
+			}
+	i++; 
+		}
+		
+
+		System.out.println("гу:"+sum );
+		
+	}
 
 }
